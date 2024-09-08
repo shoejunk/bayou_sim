6bc667ab4d3d45b2bfa18f224f1acde1124b22be
Joseph Shunk <joseph.shunk@gmail.com>
2024-09-07 20:00:46 -0700

    Simplify main.cpp

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,12 +1,4 @@
-import bayou.abl_slide;
-import bayou.board_position;
-import bayou.game_state;
-import bayou.move;
-import bayou.piece;
-import bayou.piece_library;
-import bayou.piece_state;
 import bayou.sim;
-import std.core;
 
 using namespace bayou;
 
@@ -18,7 +10,15 @@ int main()
 		return 1;
 	}
 
-	sim->place_piece(1, 0, { 0, 0 });
-	sim->try_move({ { 0, 0 }, { 0, 1 } });
+	if (!sim->place_piece(1, 0, { 0, 0 }))
+	{
+		return 1;
+	}
+
+	if (!sim->try_move({ { 0, 0 }, { 0, 1 } }))
+	{
+		return 1;
+	}
+
 	return 0;
 }
